4f7962dbf4ba2bf5e51cbcf1060957b7dc13c1f9
Safraz Razik <safrazik@gmail.com>
2020-07-14 12:50:24 +0530

    Update service worker setup

--- www/service-worker.js
+++ www/service-worker.js
@@ -6,7 +6,7 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-var appVersion = '1.8.3-49';
+var appVersion = '1.8.4-50';
 var dataVersion = '2';
 var vendorVersion = '2';
 var bgVersion = '4';
