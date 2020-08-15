80995abc97bc5f0ec44b92d87d0c07bb24880faf
Safraz Razik <safrazik@gmail.com>
2020-08-15 18:01:03 +0530

    Version 1.9.0-53

--- www/service-worker.js
+++ www/service-worker.js
@@ -6,7 +6,7 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-var appVersion = '1.8.6-52';
+var appVersion = '1.9.0-53';
 var dataVersion = '2';
 var vendorVersion = '2';
 var bgVersion = '4';
